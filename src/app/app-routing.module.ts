8fd38df03c06c900f57a1e4f81373e2b60ed4034
Sergio Gonzalez <sergio.gonzalez.gomez.contractor@bbva.com>
2023-06-20 18:52:32 +0200

    first Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharacterDetailComponent } from './pages/character-detail/character-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: `home`,
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: `characters`,
+    loadChildren: () =>
+      import('./pages/characters/characters.module').then(
+        (m) => m.CharactersModule
+      ),
+  },
+  { path: 'characters/:idCharacter', component: CharacterDetailComponent },
 
+  {
+    path: ``,
+    redirectTo: `home`,
+    pathMatch: `full`,
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CharacterDetailComponent } from './pages/character-detail/character-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CharacterDetailComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [], // TENGO QUE METER EL SERVICIO EN EL PROVIDER?????
   bootstrap: [AppComponent]
 })
 export class AppModule { }
